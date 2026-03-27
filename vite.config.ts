5c0d926c00886bfb5f56661767d1d97d552d3bb2
sw-yamasakis <yamasakis@solutionware.jp>
2026-03-27 19:10:45 +0900

    docs作成

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-    proxy: {
-      '/api': 'http://localhost:3001',
-    },
+  base: '/atamaOshiriGame/',
+  build: {
+    outDir: 'docs',
   },
 })
